65d969e66bdd48a3eed50dfca544c11657c265d0
Mateusz Kulesza <m.kulesza@pjwstk.edu.pl>
2017-11-13 19:46:09 +0100

    2.3. fetchRestaurants

--- src/app/restaurants.component.ts
+++ src/app/restaurants.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-restaurants',
   template: `
-    <p>
-      restaurants works!
-    </p>
+    <p>restaurants works!</p>
+    <div *ngFor="let restaurant of restaurants">{{restaurant.name}}</div>
   `,
   styles: []
 })
 export class RestaurantsComponent implements OnInit {
 
-  constructor() { }
+  restaurants
+
+  constructor(private http:HttpClient) { }
+
+  fetchRestaurants(){
+    this.http.get('http://localhost:3000/restaurants')
+      .subscribe(restaurants => {
+          this.restaurants = restaurants
+      })
+  }
 
   ngOnInit() {
+    this.fetchRestaurants()
   }
 
 }
