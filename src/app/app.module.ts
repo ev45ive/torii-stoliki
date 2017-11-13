f380da7dfb7d1a56771ced310561df429635c7e2
Mateusz Kulesza <m.kulesza@pjwstk.edu.pl>
2017-11-13 18:46:40 +0100

    Fix HttpClientModule import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
@@ -15,7 +15,7 @@ import { Routing } from "./app.routing";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClient,
+    HttpClientModule,
     Routing
   ],
   providers: [],
